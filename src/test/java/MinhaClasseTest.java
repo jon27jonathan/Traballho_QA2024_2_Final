84611edf430321472b4d39dcde030f404336d506
Jonathan <jsa@id.uff.br>
2024-10-27 23:06:08 -0300

    subindo atualização do POM

---
+++ src/test/java/MinhaClasseTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MinhaClasseTest {
+
+    @Test
+    public void testSoma() {
+        int resultado = 2 + 3;
+        assertEquals(5, resultado);
+    }
+}
